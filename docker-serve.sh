3313238dbf91d32ead82ca18665df3787e96eacd
Dale Karp <dale.karp@nielsen.com>
2021-07-19 10:50:20 -0400

    Resume: 2021 update

---
+++ docker-serve.sh
@@ -0,0 +1,7 @@
+# Why install Ruby when I can just build this in Docker?
+export JEKYLL_VERSION=3.8
+docker run --rm \
+	--volume="$PWD:/srv/jekyll" \
+	--publish "[::1]:4000:4000" \
+	jekyll/jekyll \
+	jekyll serve
